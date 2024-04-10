47329a8cf93098a40d4f40ca10698ae097aec013
pgul <trololo.rpgpg@gmail.com>
2024-04-10 18:45:48 +0200

    state control and error handling for .rc

--- voltage.go
+++ voltage.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
@@ -17,10 +20,23 @@ import (
 type texts struct {
 	headings []string
 }
-type rcFileCheck bool
 
-// type rcFileCreated bool
-type rcFileContent map[string]string
+// game state enum
+const (
+	stateLoadingRc = iota
+	stateRcLoadError
+	stateFormLocale
+	stateGame
+)
+
+type rcFileCheckResult struct {
+	exist bool
+	error error
+}
+type rcFileContent struct {
+	content map[string]string
+	error   error
+}
 type rcFileUpdated bool
 type newHeading int
 
@@ -45,75 +61,111 @@ func rcDirName() (string, error) {
 func checkForRcFile() tea.Msg {
 	rcPath, err := rcFileName()
 	if err != nil {
-		panic(err)
+		return rcFileCheckResult{exist: false, error: err}
 	}
 	info, err := os.Stat(rcPath)
-	if err != nil {
-		return rcFileCheck(false)
+	if errors.Is(err, fs.ErrNotExist) {
+		return rcFileCheckResult{exist: false, error: nil}
 	}
-	return rcFileCheck(!info.IsDir())
+	if err != nil {
+		return rcFileCheckResult{exist: false, error: err}
+	}
+	return rcFileCheckResult{exist: !info.IsDir(), error: nil}
 }
 
 func createRcFile() tea.Msg {
 	rcDir, err := rcDirName()
 	if err != nil {
-		panic(err)
+		return rcFileContent{
+			content: nil,
+			error:   err,
+		}
 	}
 	rcFile, err := rcFileName()
 	if err != nil {
-		panic(err)
+		return rcFileContent{
+			content: nil,
+			error:   err,
+		}
 	}
 	_, err = os.Stat(rcDir)
 	if err != nil {
 		err = os.Mkdir(rcDir, 0700)
 		if err != nil {
-			panic(err)
+			return rcFileContent{
+				content: nil,
+				error:   err,
+			}
 		}
 	}
 	resp, err := http.Get("https://raw.githubusercontent.com/pgulb/voltage/main/.voltagerc")
 	if err != nil {
-		panic(err)
+		return rcFileContent{
+			content: nil,
+			error:   err,
+		}
 	}
 	content, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return rcFileContent{
+			content: nil,
+			error:   err,
+		}
 	}
 	err = os.WriteFile(rcFile, content, 0600)
 	if err != nil {
-		panic(err)
+		return rcFileContent{
+			content: nil,
+			error:   err,
+		}
 	}
 	cfg, err := godotenv.UnmarshalBytes(content)
 	if err != nil {
-		panic(err)
+		return rcFileContent{
+			content: nil,
+			error:   err,
+		}
 	}
-	return rcFileContent(cfg)
+	return rcFileContent{
+		content: cfg,
+		error:   nil,
+	}
 }
 
 func loadRc() tea.Msg {
 	rc, err := rcFileName()
 	if err != nil {
-		panic(err)
+		return rcFileContent{
+			content: nil,
+			error:   err,
+		}
 	}
 	cfg, err := godotenv.Read(rc)
 	if err != nil {
-		panic(err)
+		return rcFileContent{
+			content: nil,
+			error:   err,
+		}
 	}
-	return rcFileContent(cfg)
+	return rcFileContent{
+		content: cfg,
+		error:   nil,
+	}
 }
 
 func updateRc(cfg map[string]string) tea.Cmd {
 	return func() tea.Msg {
 		content, err := godotenv.Marshal(cfg)
 		if err != nil {
-			panic(err)
+			return rcFileUpdated(false)
 		}
 		rc, err := rcFileName()
 		if err != nil {
-			panic(err)
+			return rcFileUpdated(false)
 		}
 		err = os.WriteFile(rc, []byte(content), 0600)
 		if err != nil {
-			panic(err)
+			return rcFileUpdated(false)
 		}
 		return rcFileUpdated(true)
 	}
@@ -139,27 +191,35 @@ func setEnglish() texts {
 	}
 }
 
-func randomHeading() int {
-	return rand.Intn(3) // 0-2
+func randomHeading(currentIndex int) int {
+	r := currentIndex
+	for r == currentIndex {
+		r = rand.Intn(3)
+	}
+	return r // 0-2
 }
 
-func RerollHeading() tea.Msg {
-	return newHeading(randomHeading())
+func RerollHeading(currentIndex int) tea.Cmd {
+	return func() tea.Msg {
+		return newHeading(randomHeading(currentIndex))
+	}
 }
 
 type model struct {
+	state               int
+	error               error
 	api                 string
 	locale              string
 	texts               texts
 	heading             string
-	index               int
+	headingIndex        int
 	formLocaleProcessed bool
 	formLocale          *huh.Form
-	rcFileChecked       bool
 }
 
 func newModel() model {
 	return model{
+		state: stateLoadingRc,
 		formLocale: huh.NewForm(huh.NewGroup(
 			huh.NewSelect[string]().
 				Key("lang").
@@ -167,46 +227,90 @@ func newModel() model {
 				Title("Choose your language / Wybierz język..."),
 		),
 		),
-		rcFileChecked: false,
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return m.formLocale.Init()
+	return tea.Batch(
+		m.formLocale.Init(),
+		checkForRcFile,
+	)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	// program exit combinations
 	case tea.KeyMsg:
-		log.Println("tea.KeyMsg (initial)")
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
-	case rcFileCheck:
-		log.Println("got rcFileCheck")
-		m.rcFileChecked = true
-		if !msg {
-			return m, createRcFile
-		} else {
-			return m, loadRc
+	}
+
+	switch m.state {
+	case stateLoadingRc:
+		switch msg := msg.(type) {
+		// creating basic .rc file or loading if detected existing
+		case rcFileCheckResult:
+			log.Println("got rcFileCheckResult")
+			if msg.error != nil {
+				log.Println(msg.error.Error())
+				m.error = msg.error
+				m.state = stateRcLoadError
+				return m, nil
+			}
+			if !msg.exist {
+				return m, createRcFile
+			} else {
+				return m, loadRc
+			}
+
+		// getting content of loaded .rc
+		case rcFileContent:
+			log.Println("got rcFileContent")
+
+			if msg.error != nil {
+				log.Println(msg.error.Error())
+				m.error = msg.error
+				m.state = stateRcLoadError
+				return m, nil
+			}
+			m.api = msg.content["VOLTAGE_API_URL"]
+			m.locale = msg.content["VOLTAGE_LOCALE"]
+			m.state = stateFormLocale
+
+			// if locale is set from .rc, no need for filling locale form
+			// locale can be empty if .rc was downloaded from github
+			if m.locale != "" {
+				//if m.locale != "" && m.formLocale.State == huh.StateNormal {
+				m.formLocale.State = huh.StateAborted
+				m.state = stateGame
+				return m, RerollHeading(m.headingIndex)
+			}
 		}
-	case rcFileContent:
-		log.Println("got rcFileContent")
-		m.api = msg["VOLTAGE_API_URL"]
-		m.locale = msg["VOLTAGE_LOCALE"]
-	}
-	if !m.rcFileChecked {
-		log.Println("requesting checkForRcFile")
-		return m, checkForRcFile
-	}
-	if m.locale != "" {
-		log.Println("disabling locale form")
-		m.formLocale.State = huh.StateAborted
-	}
-	if m.formLocale.State != huh.StateNormal {
+
+	case stateRcLoadError:
+		time.Sleep(time.Second * 10)
+		return m, tea.Quit
+
+	case stateFormLocale:
+		form, cmd := m.formLocale.Update(msg)
+		if f, ok := form.(*huh.Form); ok {
+			m.formLocale = f
+		}
+		if m.formLocale.GetString("lang") != "" {
+			// roll a heading if form was completed
+			m.state = stateGame
+			return m, tea.Sequence(cmd, RerollHeading(m.headingIndex))
+		}
+		return m, cmd
+
+	// if locale form is complete or aborted
+	case stateGame:
 		if !m.formLocaleProcessed {
 			m.formLocaleProcessed = true
+
+			// locale from form, write to .rc
 			if m.locale == "" {
 				log.Println("getting locale from filled form")
 				m.locale = m.formLocale.GetString("lang")
@@ -215,15 +319,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				cfg["VOLTAGE_LOCALE"] = m.locale
 				return m, updateRc(cfg)
 			}
-			log.Println("requesting heading for locale from rc file")
+
+			log.Println("rerolling heading")
 			if m.locale == "EN" {
 				m.texts = setEnglish()
-				return m, RerollHeading
+				return m, RerollHeading(m.headingIndex)
 			} else {
 				m.texts = setPolish()
-				return m, RerollHeading
+				return m, RerollHeading(m.headingIndex)
 			}
 		}
+
 		switch msg := msg.(type) {
 		case newHeading:
 			log.Println("got newHeading")
@@ -235,49 +341,49 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					m.texts = setPolish()
 				}
 			}
-			m.index = int(msg)
-			m.heading = m.texts.headings[m.index]
-			log.Println("heading change from cmd")
-			log.Println(m.index)
-			log.Println(m.heading)
+			m.headingIndex = int(msg)
+			m.heading = m.texts.headings[m.headingIndex]
+
+		// .rc updated
 		case rcFileUpdated:
+			if !msg {
+				log.Println("error on rcFileUpdated")
+				return m, nil
+			}
 			log.Println("got rcFileUpdated")
 			if m.locale == "EN" {
 				m.texts = setEnglish()
-				return m, RerollHeading
+				return m, nil
 			} else {
 				m.texts = setPolish()
-				return m, RerollHeading
+				return m, nil
 			}
+
 		case tea.KeyMsg:
-			log.Println("got tea.KeyMsg (later)")
 			switch msg.String() {
-			case "enter", " ":
-				return m, RerollHeading
+			case " ":
+				return m, RerollHeading(m.headingIndex)
 			}
 		}
-	} else {
-		form, cmd := m.formLocale.Update(msg)
-		if f, ok := form.(*huh.Form); ok {
-			m.formLocale = f
-		}
-		if m.formLocale.GetString("lang") != "" {
-			// create a heading if form was completed
-			return m, tea.Sequence(cmd, RerollHeading)
-		}
-		return m, cmd
 	}
+
 	return m, nil
 }
 
 func (m model) View() string {
-	if m.formLocale.State == huh.StateNormal {
+	switch m.state {
+	case stateLoadingRc:
+		return "Loading...\n"
+	case stateRcLoadError:
+		return fmt.Sprint("error!\n ", m.error.Error())
+	case stateFormLocale:
 		return m.formLocale.View()
+	case stateGame:
+		s := fmt.Sprintf("%v\n%v - %v", m.locale, m.heading, m.headingIndex)
+		return s
+	default:
+		return "unknown state: " + fmt.Sprint(m.state)
 	}
-	log.Printf("locale from View: %v\n", m.locale)
-	log.Printf("heading from View: %v\n", m.heading)
-	s := fmt.Sprintf("%v\n%v - %v", m.locale, m.heading, m.index)
-	return s
 }
 
 func main() {
